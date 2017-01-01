c4c31d67d4f95637228d1f3f486374c481d15d7f
Sarp Kaya <sarpkaya@live.com.au>
2017-01-02 00:07:47 +1000

    Added an addspec test

--- flatFS/test/file_rename_test.go
+++ flatFS/test/file_rename_test.go
@@ -321,3 +321,44 @@ func TestRenameExistingQueryWithADeleteSpecWhenFileExists(t *testing.T) {
 
 	Terminate(mountPoint)
 }
+
+
+func TestRenameExistingQueryWithAnAddSpec(t *testing.T) {
+	mountPoint := CreateFlatFS()
+	testContent1 := "Test Content"
+
+	attr1 := "foo=hello"
+	attr2 := "bar=world"
+	attr3 := "flat=fs"
+
+	addSpec := "?"
+	querySpec := "?"
+
+	exactPath := path.Join(mountPoint, attr1 + "," + attr2)
+	newPath := path.Join(mountPoint, attr1 + "," + attr2 + "," + attr3)
+	addPath := path.Join(mountPoint, addSpec + attr2)
+	queryPath := path.Join(mountPoint, querySpec + attr3)
+
+	write_to_file(exactPath, testContent1)
+	time.Sleep(time.Second * 1) // TODO fix this wait period!
+	exec_cmd("mv " + addPath + " " + queryPath)
+	time.Sleep(time.Second * 2) // TODO fix this wait period!
+	fileContent := read_from_file(newPath)
+	assert_string_equals(fileContent, testContent1, t)
+
+	fileContent = read_from_file(exactPath)
+	assert_string_equals(fileContent, "", t) //File doesn't exist
+
+	lsContent := exec_cmd("ls -l " + exactPath)
+	assert_string_not_contains(lsContent, attr1, t)
+	assert_string_not_contains(lsContent, attr2, t)
+	assert_string_not_contains(lsContent, attr3, t)
+
+	lsContent = exec_cmd("ls -l " + newPath)
+	lsContent = assert_string_contains_per_line(lsContent, []string{attr1, attr2, attr3}, t)
+	assert_string_not_contains(lsContent, attr1, t)
+	assert_string_not_contains(lsContent, attr2, t)
+	assert_string_not_contains(lsContent, attr3, t)
+
+	Terminate(mountPoint)
+}
